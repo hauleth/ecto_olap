ec519485480882a5ec396de0056514ff67ee879e
Łukasz Jan Niemier <lukasz@niemier.pl>
2017-07-05 12:27:38 +0200

    Bump patch for updated documentation

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule EctoOLAP.Mixfile do
 
   def project do
     [app: :ecto_olap,
-     version: "0.1.0",
+     version: "0.1.1",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
